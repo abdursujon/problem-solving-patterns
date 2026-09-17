d183a06b0ffd25018ba275b08476f241f6bb2acf
Sujon <abdur.sujon.dev@gmail.com>
2026-09-17 12:29:23 +0100

    remove_duplicate solution refactored

--- src/two_pointers/fast_and_slow_pointers_leetcode.py
+++ src/two_pointers/fast_and_slow_pointers_leetcode.py
@@ -1,6 +1,6 @@
 # 26. Remove Duplicates from Sorted Array and return size of the array
 class TwoPointersLeetcode:
-    def removeDuplicates(self, nums: list[int]) -> int:
+    def remove_duplicates(self, nums: list[int]) -> int:
         if not nums:
             return 0
         k = 1
@@ -8,4 +8,8 @@ class TwoPointersLeetcode:
             if(nums[i] != nums[k - 1]):
                 nums[k] = nums[i] # second and first position not same 
                 k += 1
+        del nums[k:]
         return k
+
+two_point = TwoPointersLeetcode()
+print(two_point.remove_duplicates([1, 4, 5, 5, 66]))
